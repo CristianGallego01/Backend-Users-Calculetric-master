fc3fa0319624269301bfb4411be7c1120ec0e001
Cristian Gallego <158636267+CristianGallego01@users.noreply.github.com>
2025-07-06 17:57:40 -0500

    Update SecurityConfig.java

--- src/main/java/com/irojas/demojwt/Config/SecurityConfig.java
+++ src/main/java/com/irojas/demojwt/Config/SecurityConfig.java
@@ -32,6 +32,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors()
+                .and()s
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
